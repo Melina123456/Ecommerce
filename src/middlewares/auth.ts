5ae349e49003de156430617aa9d3cbe9ec0cf339
Melina123456 <thapamelina96@gmail.com>
2024-08-06 16:52:26 +0545

    first commit

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import { UnauthorizedException } from "../exceptions/unauthorized";
+import { ErrorCode } from "../exceptions/root";
+import * as jwt from "jsonwebtoken";
+import { prismaClient } from "..";
+import { JWT_SECRET } from "../secrets";
+
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // 1. extract token from request headers
+  const token = req.headers.authorization || "";
+  //2.if token is not present, throw unauthorized exception
+  if (!token) {
+    next(new UnauthorizedException("unauthorized", ErrorCode.UNAUTHORIZED));
+  }
+  try {
+    //3. if token is present, verify and extract the payload
+    const payload = jwt.verify(token, JWT_SECRET) as any;
+    // 4. to get user from the payload
+    const user = await prismaClient.user.findFirst({
+      where: { id: payload.userId },
+    });
+    if (!user) {
+      next(new UnauthorizedException("unauthorized", ErrorCode.UNAUTHORIZED));
+    }
+    // 5. to attach the user to the current request object
+    req["user"] = user;
+    next();
+  } catch (error) {
+    next(new UnauthorizedException("unauthorized", ErrorCode.UNAUTHORIZED));
+  }
+};
+export default authMiddleware;
---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { login, me, signup } from "../controllers/auth.controller";
+import { errorHandler } from "../error_handler";
+import authMiddleware from "../middlewares/auth";
+
+const authRoutes: Router = Router();
+
+authRoutes.post("/signup", errorHandler(signup));
+
+authRoutes.post("/login", errorHandler(login));
+
+authRoutes.get("/me", [authMiddleware], errorHandler(me));
+
+export default authRoutes;
