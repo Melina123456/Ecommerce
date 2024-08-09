0c0418f278a33ddd0434a936b099c9878aa2ec18
Melina123456 <thapamelina96@gmail.com>
2024-08-09 15:36:21 +0545

    new functions in order controller

--- src/routes/orders.routes.ts
+++ src/routes/orders.routes.ts
@@ -3,16 +3,35 @@ import { errorHandler } from "../error_handler";
 import authMiddleware from "../middlewares/auth";
 import {
   cancelOrder,
+  changeStatus,
   createOrder,
   getOrderById,
+  listAllOrders,
   listOrders,
+  listUserOrders,
 } from "../controllers/orders.controller";
+import adminMiddleware from "../middlewares/admin";
 
 const orderRoutes: Router = Router();
 
 orderRoutes.post("/", [authMiddleware], errorHandler(createOrder));
 orderRoutes.get("/", [authMiddleware], errorHandler(listOrders));
 orderRoutes.patch("/:id/cancel", [authMiddleware], errorHandler(cancelOrder));
+orderRoutes.get(
+  "/index",
+  [authMiddleware, adminMiddleware],
+  errorHandler(listAllOrders)
+);
+orderRoutes.get(
+  "/users/:id",
+  [authMiddleware, adminMiddleware],
+  errorHandler(listUserOrders)
+);
+orderRoutes.patch(
+  "/:id/status",
+  [authMiddleware, adminMiddleware],
+  errorHandler(changeStatus)
+);
 orderRoutes.get("/:id", [authMiddleware], errorHandler(getOrderById));
 
 export default orderRoutes;
