d8f09252330b77878a16086bc4bfc69000403ab4
Melina123456 <thapamelina96@gmail.com>
2024-08-09 15:05:11 +0545

    new functions added

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,10 +3,14 @@ import { errorHandler } from "../error_handler";
 import authMiddleware from "../middlewares/auth";
 import {
   addAddress,
+  changeUserRole,
   deleteAddress,
+  getUserById,
   listAddresses,
+  listUsers,
   updateUser,
 } from "../controllers/users";
+import adminMiddleware from "../middlewares/admin";
 
 const userRoutes: Router = Router();
 
@@ -21,5 +25,16 @@ userRoutes.delete(
 userRoutes.get("/address", [authMiddleware], errorHandler(listAddresses));
 
 userRoutes.put("/update", [authMiddleware], errorHandler(updateUser));
+userRoutes.patch(
+  "/:id/role",
+  [authMiddleware, adminMiddleware],
+  errorHandler(changeUserRole)
+);
+userRoutes.get("/", [authMiddleware, adminMiddleware], errorHandler(listUsers));
+userRoutes.get(
+  "/:id",
+  [authMiddleware, adminMiddleware],
+  errorHandler(getUserById)
+);
 
 export default userRoutes;
